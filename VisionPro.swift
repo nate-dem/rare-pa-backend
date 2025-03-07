327b461b6a19e0d93491dc73f92598208740d4fd
nate-dem <natedem@stanford.edu>
2025-03-06 17:51:24 -0800

    Finished and updated the Firebase to AVP file

--- VisionPro.swift
+++ VisionPro.swift
@@ -1,5 +1,5 @@
 //
-//  VisionPro.swift
+//  VisionProWorkout.swift
 //  Backend
 //
 //  Created by nate demchak on 2/27/25.
@@ -26,5 +26,44 @@ class VisionProWorkout: ObservableObject {
     init() {
         let databaseURL = "https://rare-pa-backend-default-rtdb.firebaseio.com/"
         self.databaseRef = Database.database(url: databaseURL).reference()
+        
+        // Important: Call the setup function to activate listeners
+        setUpHeartRateListener()
+    }
+    
+    private func setUpHeartRateListener() {
+        databaseRef.child("sessions").observe(.childAdded) { [weak self] snapshot in
+            // makes sure we don't have memory leaks
+            guard let self = self else { return }
+            self.currentSessionID = snapshot.key
+            self.isSessionActive = true
+            
+            if let curListener = self.heartRateListener {
+                self.databaseRef.removeObserver(withHandle: curListener)
+            }
+            
+            self.heartRateListener = self.databaseRef
+                .child("sessions")
+                .child(self.currentSessionID)
+                .child("heartRate")
+                .observe(.childAdded) { [weak self] heartRateSnapshot in
+                    guard
+                        let self = self,
+                        let heartRateData = heartRateSnapshot.value as? [String: Any],
+                        let heartRate = heartRateData["heartRate"] as? Double
+                    else { return }
+                    
+                    DispatchQueue.main.async {
+                        self.currentHeartRate = heartRate
+                    }
+                }
+        }
+    }
+        
+    deinit {
+        if let listener = heartRateListener {
+            databaseRef.removeObserver(withHandle: listener)
+        }
+        databaseRef.removeAllObservers()
     }
 }
